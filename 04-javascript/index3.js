a858c6194b85216b2c927dfacea7269110aab130
Taehoon <t11kim@yonsei.ac.kr>
2023-07-26 14:53:15 +0900

    study: js 자료형, 형변환

--- 04-javascript/index3.js
+++ 04-javascript/index3.js
@@ -1,4 +1,4 @@
-console.log("connected!")
+console.log('connected!');
 
 // 변수 선언: 변수 정의
 // 변수 할당: = 연산자를 이용해 값을 넣음
@@ -17,37 +17,37 @@ console.log("connected!")
 // - let 키워드로는 변수 중복 선언이 불가능하지만, 재할당이 가능하다.
 
 // 변수 선언 키워드 1. var
-var a // 변수 선언
-console.log(a) // undefined: 값을 할당하지 않음
-a = 2 // 변수 할당
-console.log(a) // 변수 값 출력 (2)
-var a2 = 4 // 변수 선언 & 할당
-console.log(a2)
+var a; // 변수 선언
+console.log(a); // undefined: 값을 할당하지 않음
+a = 2; // 변수 할당
+console.log(a); // 변수 값 출력 (2)
+var a2 = 4; // 변수 선언 & 할당
+console.log(a2);
 
 // var 변수의 이상한 점
 // 변수를 선언하기 전에 값을 할당
 // -> 장점같이 보일 수 있으나, 유지보수할 때 최악
-aa = 123
-console.log(aa)
+aa = 123;
+console.log(aa);
 
 // 변수 선언 키워드 2. let
-let b // 변수 선언
-console.log(b) // undefined: 변수 선언만 하고 값을 할당하지 않았으므로 초기값(undefined) 출력
-b = 7 // 변수 할당
-console.log(b) // 변수 값 출력 (7)
+let b; // 변수 선언
+console.log(b); // undefined: 변수 선언만 하고 값을 할당하지 않았으므로 초기값(undefined) 출력
+b = 7; // 변수 할당
+console.log(b); // 변수 값 출력 (7)
 
-let b2 = 77 // 변수 선언 & 할당
-console.log(b2)
+let b2 = 77; // 변수 선언 & 할당
+console.log(b2);
 // let b2 = 78; 이렇게는 할당할 수 없음. 중복 선언 불가
-b2 = 78 // 변수 값 재할당
-console.log(b2)
+b2 = 78; // 변수 값 재할당
+console.log(b2);
 
 // 변수 선언 키워드 3. const
 // -> 변수 선언과 할당이 동시에!!!! (반드시)
 // -> 변하지 않는 값을 변수에 저장할 때 사용 (상수)
 // const c; -> error: const 키워드는 선언과 할당 반드시 동시에 이뤄져야함!
-const c = 3
-console.log(c)
+const c = 3;
+console.log(c);
 // c = 33 -> error: const 변수는 재할당 불가!
 
 // 변수 이름 규칙 (=> 식별자 규칙)
@@ -74,39 +74,145 @@ console.log(c)
 // 1. string (문자열)
 // : 텍스트 정보, 따옴표로 감싸야 함
 // : 숫자, 특수문자도 따옴표 안에 포함되면 문자열로 볼 수 있다.
-let text = '안녕하세요!'
-let text2 = '하이!'
-let gender = '남자'
-console.log(text)
-console.log(text2)
-console.log(gender)
-console.log(text+text2+gender)
+let text = '안녕하세요!';
+let text2 = '하이!';
+let gender = '남자';
+console.log(text);
+console.log(text2);
+console.log(gender);
+console.log(text + text2 + gender);
 // 템플릿 리터럴(es6) 백틱(`)와 달러, 중괄호(${}) 조합으로 사용
 // : 문자열 내에서 변수와 상수를 간결히 표현
-console.log(`${text} 저는 태훈입니다. 저의 성별은 ${gender}입니다!`)
+console.log(`${text} 저는 태훈입니다. 저의 성별은 ${gender}입니다!`);
 
 // 2. number (숫자형)
 // 정수, 실수
 // 연산 가능 (+, -, /, *)
-let num = 100
-let num2 = 0.12
-console.log(num + num2)
-console.log(num, num2)
+let num = 100;
+let num2 = 0.12;
+console.log(num + num2);
+console.log(num, num2);
 
 // 3. boolean (불린)
 // true, false
-let isWater = true
-let hasApple = false
-console.log(isWater, hasApple)
+let isWater = true;
+let hasApple = false;
+console.log(isWater, hasApple);
 
 // 4. null (빈 값)
 // "값이 없음"을 의도적으로 명시
-let temp = null
-console.log(temp)
-temp = 'Hello'
-console.log(temp)
+let temp = null;
+console.log(temp);
+temp = 'Hello';
+console.log(temp);
 
 // 5. undefined
 // 값도 없고, 타입도 지정되지 않은 상태
 let x; // 변수 선언만 했을 때 초기 값으로 undefined 할당
-console.log(x) // undefined
+console.log(x); // undefined
+
+// 6. Array (배열)
+// 배열의 이름: fruits
+// 배열의 원소(아이템): 배열 안에 있는 데이터 하나하나
+// 배열의 위치(index): 0부터 시작
+// 배열의 길이(크기): 원소의 개수와 동일
+const fruits = ['orange', 'pineapple', 'grape', 'apple'];
+// const fruits2 = new Array ('orange', 'pineapple', 'grape', 'apple')
+console.log(fruits[0]); // fruits 배열의 위치가 0인 원소 출력
+
+// js에서는 배열 원소의 자료형이 달라도 괜찮다!
+const data = [1, 'allie', false, null, undefined];
+console.log(data);
+
+// Array 안에 Array 들어갈 수 있음 (중첩 가능) => 2차원 배열
+const korean = [
+  ['가', '나', '다'],
+  ['라', '마', '바'],
+  ['사', '아', '자'],
+];
+
+console.log(korean);
+console.log(korean[0]);
+console.log(korean[0][2]);
+
+// 퀴즈 korean 배열에서 '가자' 글씨 출력하기!
+console.log(korean[0][0] + korean[2][2]);
+
+// 퀴즈 3차원 배열에서 숫자 8 출력
+const nums = [
+  [
+    [1, 2, 3],
+    [4, 5, 6],
+  ],
+  [
+    [7, 8, 9],
+    [10, 11, 12],
+  ],
+];
+console.log(nums[1][0][1]);
+
+// 7. object (key: value)
+const cat = {
+  name: '나비',
+  age: 1,
+  isCute: true,
+  mew: function () {
+    return '냐옹';
+  },
+};
+console.log(cat);
+
+// object의 속성(key)에 접근하는 방법
+// (1) 점 표기법
+console.log(cat.name);
+// (2) 대괄호 표기법 ([])
+console.log(cat['name']);
+// key가 변수에 저장되어 있을 때 주로 사용
+const tempVal = 'name';
+console.log(cat[tempVal]); // cat['name'] => cat.name과 동일
+
+// typeof : 자료형을 확인할 수 있는 키워드
+// 사용방식 1. typeof(x) 2. typeof x
+console.log(typeof '문자'); // string
+console.log(typeof 12345); // number
+console.log(typeof 3.14); // number
+console.log(typeof true); // boolean
+console.log(typeof false); // boolean
+console.log(typeof null); // object ** js 에서 공식적으로 인정한 언어 자체의 오류
+console.log(typeof undefined); // undefined
+console.log(typeof fruits); // object ** Array도 object이다
+console.log(typeof cat); // object
+
+// 형 변환
+// 성적 평균 구하는 프로그램 만들기
+// let mathScore = prompt('수학점수 입력');
+// console.log(mathScore, typeof mathScore);
+// let engScore = prompt('영어점수 입력');
+// console.log(engScore, typeof engScore);
+// let avg = (mathScore + engScore) / 2;
+// console.log(`수학과 영어의 평균 점수는 ${avg}점 입니다!`);
+
+// 1. String(): 문자열로 형변환
+let str1 = true;
+let str2 = 123;
+let str3 = null;
+console.log(String(str1), typeof String(str1));
+console.log(String(str2), typeof String(str2));
+console.log(String(str3), typeof String(str3));
+console.log(str1.toString(), typeof str1.toString());
+
+// 2. Number(): 숫자로 형변환
+let n1 = true;
+let n2 = false;
+let n3 = '123.9';
+console.log(Number(n1), typeof Number(n1)); // 1
+console.log(Number(n2), typeof Number(n2)); // 0
+console.log(Number(n3), typeof Number(n3)); // 123.9
+console.log(parseInt(n3, 10)); // n3 값을 10진수의 정수(int)로 바꾸겠다 => 123
+console.log(parseFloat(n3)); // n3 값을 실수(float)로 바꾸겠다 => 123.9
+console.log(Number(n3)); // n3 값을 실수(float)로 바꾸겠다 => 123.9
+
+// +) null, undefined를 숫자로 바꾼다면?
+console.log(Number(undefined), typeof Number(undefined)); // NaN, number
+// Not-A-Number (숫자가 아님)
+console.log(Number(null), typeof Number(null)); // 0, number
---
+++ 04-javascript/practice/1.js
@@ -0,0 +1,6 @@
+const introduce = {
+  name: 'Taehoon',
+  job: 'student',
+  interests: ['youtube', 'music', 'game'],
+};
+console.log(introduce);
