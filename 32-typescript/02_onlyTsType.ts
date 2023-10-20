605427abf4b0787fa812b69b3734827ec018dd36
Taehoon <mysoul98@gmail.com>
2023-10-20 15:52:22 +0900

    study: typescript 문법

--- 32-typescript/02_onlyTsType.ts
+++ 32-typescript/02_onlyTsType.ts
@@ -52,3 +52,121 @@ val = { name: 'sesac' };
 let val2;
 val2 = false;
 val2 = '바이';
+
+////////////////////////////////////
+// 1. interface
+interface Student {
+  name: string;
+  isPassed: boolean;
+}
+
+const student1: Student = {
+  name: 'jinheyong',
+  isPassed: true,
+  // addr: 'seoul',
+};
+
+const student2: object = {
+  name: 'jinheyong',
+  isPassed: true,
+  addr: 'seoul',
+};
+
+// interface 상속
+// A+, A, B, C, D, F
+// type Score = 'A+' | 'A' | 'B' | 'C' | 'D' | 'F';
+enum Score {
+  Aplus = 'A+',
+  A = 'A',
+  B = 'B',
+  C = 'C',
+  D = 'D',
+  F = 'F',
+}
+
+interface BaseballClub extends Student {
+  position: string;
+  height: number;
+  readonly backNumber?: number; // ?: 있어도 되고 없어도 됨
+  // [grade: number]: string;
+  [grade: number]: Score;
+}
+
+const otani: BaseballClub = {
+  name: 'ontani',
+  isPassed: true,
+  position: 'pitcher',
+  height: 193,
+  backNumber: 17,
+  1: Score.A, // 학년: 점수
+  // 2: 'NP', // error
+};
+console.log(otani);
+
+otani.position = '투수';
+otani['height'] = 200;
+// otani.backNumber = 16; // error : backNumber readonly
+console.log(otani);
+
+// type vs. enum
+type Bp1 = 500 | 700 | 1000;
+enum Bp2 {
+  SM = 500,
+  MD = 700,
+  LG = 1000,
+}
+
+const width1: Bp1 = 500;
+const width2: Bp2 = Bp2.SM;
+
+// 교차 타입
+interface Toy {
+  name: string;
+  start(): void;
+}
+
+interface Car {
+  name: string;
+  color: string;
+  price: number;
+}
+
+const toyCar: Toy & Car = {
+  name: 'tayo',
+  start() {
+    console.log('출발~~');
+  },
+  color: 'blue',
+  price: 5000,
+};
+
+// 실습
+enum Category {
+  Action = '액션',
+  Role = '롤플레잉',
+}
+
+type Platform = '모바일' | 'PC';
+
+interface Game {
+  title: string;
+  price: number;
+  desc?: string;
+  category: Category;
+  platform: Platform;
+}
+
+const heroGame_A: Game = {
+  title: 'DC 언체인드',
+  price: 50000,
+  desc: 'DC 히어로 & 빌런 각각의 개성은 물론, 액션의 재미까지!',
+  category: Category.Action,
+  platform: '모바일',
+};
+
+const heroGame_B: Game = {
+  title: 'MARVEL 퓨처파이트',
+  price: 65000,
+  category: Category.Role,
+  platform: 'PC',
+};
